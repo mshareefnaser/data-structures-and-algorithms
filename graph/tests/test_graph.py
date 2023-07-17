2d6e856b59e25bfbce902751c65f024cffb7ea5b
shnorlaxzz <mshnas56@gmail.com>
2023-07-17 14:31:11 +0000

    CC36 done

--- graph/tests/test_graph.py
+++ graph/tests/test_graph.py
@@ -15,7 +15,7 @@ def test_add_edge():
     graph.add_edge(vertex1, vertex2)
     neighbors = graph.get_neighbours(vertex1)
     assert len(neighbors) == 1
-    assert neighbors[0].vertex == vertex2
+    assert neighbors[0] == vertex2
 
 def test_get_vertices():
     """Test retrieving all vertices from the graph."""
@@ -35,18 +35,18 @@ def test_get_neighbours():
     graph.add_edge(vertex1, vertex2)
     neighbors = graph.get_neighbours(vertex1)
     assert len(neighbors) == 1
-    assert neighbors[0].vertex == vertex2
+    assert neighbors[0] == vertex2
 
-def test_get_neighbours_with_weight():
-    """Test retrieving neighbors with weights of a vertex in the graph."""
-    graph = Graph()
-    vertex1 = graph.add_vertex(1)
-    vertex2 = graph.add_vertex(2)
-    graph.add_edge(vertex1, vertex2)
-    neighbors = graph.get_neighbours(vertex1)
-    assert len(neighbors) == 1
-    assert neighbors[0].vertex == vertex2
-    assert neighbors[0].weight == 0  
+# def test_get_neighbours_with_weight():
+#     """Test retrieving neighbors with weights of a vertex in the graph."""
+#     graph = Graph()
+#     vertex1 = graph.add_vertex(1)
+#     vertex2 = graph.add_vertex(2)
+#     graph.add_edge(vertex1, vertex2)
+#     neighbors = graph.get_neighbours(vertex1)
+#     assert len(neighbors) == 1
+#     assert neighbors[0].vertex == vertex2
+#     assert neighbors[0].weight == 0  
 
 def test_get_size():
     """Test retrieving the size (number of vertices) of the graph."""
@@ -54,3 +54,27 @@ def test_get_size():
     vertex1 = graph.add_vertex(1)
     vertex2 = graph.add_vertex(2)
     assert graph.get_size() == 2
+
+def test_bfs():
+    """Test breadth-first traversal of the graph."""
+    g = Graph()
+    pandora = g.add_vertex('Pandora')
+    arendelle = g.add_vertex('Arendelle')
+    metroville = g.add_vertex('Metroville')
+    monstroplolis = g.add_vertex('Monstroplolis')
+    narnia = g.add_vertex('Narnia')
+    naboo = g.add_vertex('Naboo')
+
+    g.add_edge(pandora, arendelle)
+    g.add_edge(arendelle, metroville)
+    g.add_edge(arendelle, monstroplolis)
+    g.add_edge(metroville, monstroplolis)
+    g.add_edge(metroville, narnia)
+    g.add_edge(metroville, naboo)
+    g.add_edge(monstroplolis, naboo)
+    g.add_edge(narnia, naboo)
+
+    actual = g.breadth_first(pandora)
+    expected = ['Pandora', 'Arendelle', 'Metroville', 'Monstroplolis', 'Narnia', 'Naboo']
+    assert actual == expected
+
